1c8f1c0bd47b49d823559381980fa6ff97f98b93
azlan.hussein <vhussein80@gmail.com>
2017-08-04 16:41:46 +0800

    update

---
+++ spring-constructor-args-demo/src/main/java/com/azlan/springdemo/Organization.java
@@ -0,0 +1,28 @@
+package com.azlan.springdemo;
+
+public class Organization {
+	
+	private String companyName;
+	private int yearOfIncorporation;
+	
+//	public Organization(){}
+	
+	public Organization(String companyName, int yearOfIncorporation) {
+		this.companyName = companyName;
+		this.yearOfIncorporation = yearOfIncorporation;
+	}
+
+
+	public void corporateSlogan()
+	{
+		String slogan = "We build the ultimate driving machines";
+		System.out.println(slogan);
+	}
+
+
+	@Override
+	public String toString() {
+		return "Organization [companyName=" + companyName + ", yearOfIncorporation=" + yearOfIncorporation + "]";
+	}
+
+}
